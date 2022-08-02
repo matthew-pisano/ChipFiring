cd9460184b450f2f3283172ec8e968f4605a4551
Matthew Pisano <matthewpisano14@gmail.com>
2022-08-01 20:03:27 -0400

    SmithNormalForm

--- algorithms.py
+++ algorithms.py
@@ -7,22 +7,25 @@ class Divisor:
     def __init__(self, chips: list[int]):
         self.rawChips = chips
         # vertical vector for multiplication
-        self.divisor = np.array(chips).transpose()
+        self.divisor = np.array([chips]).transpose()
         self.iterPlace = 0
 
     def deg(self):
         """Return the degree of the divisor"""
         vsum = 0
         for v in self.divisor:
-            vsum += v
+            vsum += v[0]
         return vsum
 
     def isEffective(self):
         """If the divisor is winning"""
         for v in self.divisor:
-            if v < 0:
+            if v[0] < 0:
                 return False
         return True
+
+    def config(self, vertex):
+        return np.delete(self.divisor, vertex, axis=0)
 
     def copy(self):
         return Divisor(self.rawChips.copy())
@@ -30,14 +33,17 @@ class Divisor:
     def __int__(self):
         return self.deg()
 
+    def __add__(self, other):
+        self.divisor = np.add(self.divisor, other)
+
     def __len__(self):
         return len(self.divisor)
 
     def __getitem__(self, item: int) -> int:
-        return self.divisor[item]
+        return self.divisor[item][0]
 
     def __setitem__(self, key, value):
-        self.divisor[key] = value
+        self.divisor[key][0] = value
 
     def __iter__(self):
         self.iterPlace = 0
@@ -48,13 +54,133 @@ class Divisor:
         if place >= len(self.divisor):
             raise StopIteration()
         self.iterPlace += 1
-        return self.divisor[place]
+        return self.divisor[place][0]
 
     def __str__(self):
-        return f"Divisor({self.divisor})"
+        return f"Divisor({np.array(self.divisor.transpose()[0])})"
 
     def __repr__(self):
-        return f"Divisor({self.divisor})"
+        return f"Divisor({np.array(self.divisor.transpose()[0])})"
+
+    def __eq__(self, other):
+        for idx, v in enumerate(self.divisor):
+            if self[idx] != other[idx]:
+                return False
+        return True
+
+
+class Utils:
+    @classmethod
+    def smithNormalForm(cls, matrix: np.ndarray):
+        matrixClone = matrix.copy()
+
+        def exchangeRows(other: np.ndarray, i: int, j: int):
+            matrixClone[[i, j]] = matrixClone[[j, i]]
+            other[[i, j]] = other[[j, i]]
+
+        def exchangeCols(other: np.ndarray, i: int, j: int):
+            matrixClone[:, [i, j]] = matrixClone[:, [j, i]]
+            other[:, [i, j]] = other[:, [j, i]]
+
+        def addRows(other: np.ndarray, i: int, j: int, scale=1):
+            matrixClone[i, :] = (matrixClone[i, :] + scale * matrixClone[j, :])
+            other[i, :] = (other[i, :] + scale * other[j, :])
+
+        def addCols(other: np.ndarray, i: int, j: int, scale=1):
+            matrixClone[:, i] = (matrixClone[:, i] + scale * matrixClone[:, j])
+            other[:, i] = (other[:, i] + scale * other[:, j])
+
+        def scaleRow(other: np.ndarray, i: int, scale):
+            matrixClone[i, :] = scale * matrixClone[i, :]
+            other[i, :] = scale * other[i, :]
+
+        def scaleCol(other: np.ndarray, i: int, scale=1):
+            matrixClone[:, i] = scale * matrixClone[:, i]
+            other[:, i] = scale * other[:, i]
+
+        m, n = matrixClone.shape
+
+        def minAij(s):
+            """Find the minimum non-zero element below and to the right of matrix[s][s]"""
+            element = [s, s]
+            globalMin = max(max([abs(x) for x in matrixClone[j][s:]]) for j in range(s, m))
+            for i in (range(s, m)):
+                for j in (range(s, n)):
+                    if matrixClone[i][j] != 0 and abs(matrixClone[i][j]) <= globalMin:
+                        element = [i, j]
+                        globalMin = abs(matrixClone[i][j])
+            return element
+
+        def isLone(s):
+            """Checks if matrix[s][s] is the only non-zero in col s below matrix[s][s] and the only
+            non-zero in row s to the right of matrix[s][s]"""
+            if [matrixClone[s][x] for x in range(s + 1, n) if matrixClone[s][x] != 0] + [matrixClone[y][s]
+                    for y in range(s + 1, m) if matrixClone[y][s] != 0] == []:
+                return True
+            else:
+                return False
+
+        def findNonDivisible(s):
+            """Finds the first element which is not divisible by matrix[s][s]"""
+            for x in range(s + 1, m):
+                for y in range(s + 1, n):
+                    if matrixClone[x][y] % matrixClone[s][s] != 0:
+                        return x, y
+            return None
+
+        p = np.identity(m)
+        q = np.identity(n)
+        for s in range(min(m, n)):
+            while not isLone(s):
+                # Get min location
+                i, j = minAij(s)
+                exchangeRows(p, s, i)
+                exchangeCols(q, s, j)
+                for x in range(s + 1, m):
+                    if matrixClone[x][s] != 0:
+                        k = matrixClone[x][s] // matrixClone[s][s]
+                        addRows(p, x, s, -k)
+                for x in range(s + 1, n):
+                    if matrixClone[s][x] != 0:
+                        k = matrixClone[s][x] // matrixClone[s][s]
+                        addCols(q, x, s, -k)
+                if isLone(s):
+                    res = findNonDivisible(s)
+                    if res:
+                        x, y = res
+                        addRows(p, s, x, 1)
+                    else:
+                        if matrixClone[s][s] < 0:
+                            scaleRow(p, s, -1)
+
+        return np.matmul(p, np.matmul(matrix, q)), p, q
+
+    @classmethod
+    def coKernel(cls, matrix: np.ndarray, divisor: Divisor | np.ndarray = None):
+        """Returns the polynomial, invariant factors, and rank of the coKernel of the given matrix"""
+        smith, p, q = cls.smithNormalForm(matrix)
+        print(smith)
+        if not divisor:
+            product = p
+        else:
+            product = np.matmul(p, divisor)
+        infs = []
+        invFactors = []
+        delModifier = 0
+        for i in range(len(smith)):
+            if smith[i][i] == 1 or np.all((smith[i] == 0)):
+                product = np.delete(product, i + delModifier, axis=0)
+                delModifier -= 1
+                if np.all((smith[i] == 0)):
+                    infs.append(float("inf"))
+            else:
+                invFactors.append(smith[i][i])
+        product = [np.atleast_1d(layer).tolist() for layer in product]
+        if len(infs) > 0:
+            product.append(infs)
+
+        # polynomial, invariant factors, rank
+        return product, invFactors, len(infs)
 
 
 class Graph:
@@ -70,14 +196,17 @@ class Graph:
                 laplacian[i][j] = self.degree(i) if i == j else -self.commonEdges(i, j)
         return laplacian
 
+    def reducedLaplacian(self, vertex):
+        return np.delete(np.delete(self.laplacian, vertex, axis=0), vertex, axis=1)
+
     def edgeSet(self, vertex=-1):
         """Returns either the edge set for a vertex or the edge set for the whole graph"""
         if vertex < 0:
-            return [(v, w) for w in range(0, len(self.matrix)) for v in range(0, len(self.matrix))
-                    for _ in range(0, self.matrix[v][w]) if v < w]
+            return [(v, w, self.matrix[v][w]) for w in range(0, len(self.matrix))
+                    for v in range(0, len(self.matrix)) if v != w and self.matrix[v][w] > 0]
 
-        return [(vertex, w) for w in range(0, len(self.matrix))
-                for _ in range(0, self.matrix[vertex][w]) if vertex != w]
+        return [(vertex, w, self.matrix[vertex][w])
+                for w in range(0, len(self.matrix)) if vertex != w and self.matrix[vertex][w] > 0]
 
     def commonEdges(self, v, w):
         return self.matrix[v][w]
@@ -102,15 +231,29 @@ class Graph:
         edges = self.edgeSet(v)
         return (v, w) in edges or (w, v) in edges
 
-    def lend(self, divisor: Divisor, vertexes: list | int, amount):
+    def lend(self, divisor: Divisor, vertexes: list | np.ndarray | int, amount, forceLegal=False):
         """Lend from a vertex amount number of times.
         Borrows if amount is negative"""
         if type(vertexes) is int:
             vertexes = [vertexes]
 
-        moves = [0] * len(divisor)
+        moves = None
         for vertex in vertexes:
             adj = self.adjacencySet(vertex)
+            if len(adj) == 0:
+                continue
+            # If only legal moves can be made
+            if forceLegal:
+                adjIllegal = False
+                for w in adj:
+                    if amount < 0 and divisor[w] + amount < 0:
+                        adjIllegal = True
+                        break
+                # Skip this move if it will cause any vertex to go into an illegal state
+                if adjIllegal or (amount > 0 and divisor[vertex] - amount * len(adj)) < 0:
+                    continue
+            if moves is None:
+                moves = [0] * len(divisor)
             divisor[vertex] -= amount * len(adj)
             moves[vertex] -= amount * len(adj)
             for w in adj:
@@ -119,22 +262,50 @@ class Graph:
 
         return moves
 
-    def visualize(self, divisor: Divisor):
-        """Creates a plot representation of the grapg"""
+    def jac(self, divisor: Divisor = None, vertex=0):
+        """Returns an element of Jac(G) that is LinEq to the given divisor"""
+        return Utils.coKernel(self.reducedLaplacian(vertex), divisor.config(vertex) if divisor else None)
+
+    def pic(self, divisor: Divisor = None):
+        """Returns an element of Pic(G) that is LinEq to the given divisor"""
+        return Utils.coKernel(self.laplacian, divisor)
+
+    def visualize(self, divisor: Divisor = None, withWeights=False):
+        """Creates a plot representation of the graph"""
         labels = {}
-        for i in range(0, len(divisor)):
-            labels[i] = f"{i}: {divisor[i]}"
-        G = nx.Graph()
-        G.add_edges_from(self.edgeSet())
-        nx.draw_networkx(G, labels=labels, node_size=700)
+        for i in range(0, len(self.matrix)):
+            labels[i] = f"{i}"+(f": {divisor[i]}" if divisor else "")
+        G = nx.DiGraph()
+        G.add_weighted_edges_from(self.edgeSet())
+        pos = nx.circular_layout(G)
+        if withWeights:
+            edgeLabels = nx.get_edge_attributes(G, 'weight')
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=edgeLabels)
+        nx.draw_networkx(G, pos, labels=labels, node_size=700)
         plt.show()
+
+    def __len__(self):
+        return len(self.matrix)
+
+
+def fireCycle(graph: Graph, divisor: Divisor, mutate=False):
+    """Fires all vertices until none can fire anymore or the original
+    configuration has been reached"""
+    original = divisor.copy()
+    if not mutate:
+        divisor = divisor.copy()
+    moveSet = graph.lend(divisor, list(range(len(divisor))), 1, forceLegal=True)
+    while divisor != original and moveSet is not None and moveSet != [0]*len(moveSet):
+        moveSet = graph.lend(divisor, list(range(len(divisor))), 1, forceLegal=True)
+
+    return divisor
 
 
 def greedy(graph: Graph, divisor: Divisor, mutate=False):
     """Finds an equivalent, effective divisor to the one given using
     the greedy algorithm"""
     if divisor.deg() < 0:
-        return False
+        return False, None, None
     marked = []
     moves = []
     if not mutate:
@@ -144,7 +315,10 @@ def greedy(graph: Graph, divisor: Divisor, mutate=False):
         if len(marked) < len(divisor):
             for i in range(0, len(divisor)):
                 if divisor[i] < 0:
-                    moves.append(graph.lend(divisor, i, -1))
+                    move = graph.lend(divisor, i, -1)
+                    if move is None:
+                        return False, None, None
+                    moves.append(move)
                     if i not in marked:
                         marked.append(i)
                     break
@@ -154,7 +328,7 @@ def greedy(graph: Graph, divisor: Divisor, mutate=False):
 
 
 def dhar(graph: Graph, divisor: Divisor, source: int, burned: list[int] = None):
-    """Determines if a divisor has a super-stable configuration"""
+    """Determines if a divisor has a super-stable configuration about a source vertex"""
     if burned is None:
         burned = [source]
 
@@ -169,11 +343,15 @@ def dhar(graph: Graph, divisor: Divisor, source: int, burned: list[int] = None):
             burned.append(v)
             dhar(graph, divisor, v, burned)
 
-    return len(burned) == len(divisor)
+    return len(burned) == len(divisor), [i for i in range(0, len(divisor)) if i not in burned], burned
 
 
-def qReduced(graph: Graph, divisor: Divisor, mutate=False):
-    q = 0
+def qReduced(graph: Graph, divisor: Divisor, q=0, mutate=False):
+    """Finds an equivalent, effective divisor to the one given using
+    the q-reduced divisors algorithm"""
+    if divisor.deg() < 0:
+        return False, None, None
+
     if not mutate:
         divisor = divisor.copy()
     moves = []
@@ -192,9 +370,31 @@ def qReduced(graph: Graph, divisor: Divisor, mutate=False):
                 moves.append(graph.lend(divisor, i, 1))
 
     # Stage 2: Relief
-    legals = [i for i in range(0, len(divisor)) if i != q]
-    while divisor[q] < 0 and len(legals) > 0:
-        lIdx = 0
+    isSuperstable = True
+    while divisor[q] < 0 and isSuperstable:
+        isSuperstable, legals, nonLegal = dhar(graph, divisor, q)
+        graph.lend(divisor, legals, 1)
+
+    if divisor[q] < 0:
+        return False, None, None
+    return True, moves, divisor
+
+
+def debtReduction(graph: Graph, divisor: Divisor, q=0, mutate=False):
+    if not mutate:
+        divisor = divisor.copy()
+    firingScript = np.floor(np.matmul(np.linalg.inv(graph.reducedLaplacian(q)), divisor.config(q)))
+    firedCfg = divisor.config(q) - np.matmul(graph.reducedLaplacian(q), firingScript)
+    oldDeg = divisor.deg()
+    for i in range(0, len(divisor)):
+        if i != q:
+            divisor[i] = firedCfg[i if i < q else i - 1]
+    divisor[q] = oldDeg - np.sum(firedCfg)
+
+    return divisor
+
+"""
+lIdx = 0
         while lIdx < len(legals):
             adjSet = graph.adjacencySet(legals[lIdx])
             count = 0
@@ -205,11 +405,24 @@ def qReduced(graph: Graph, divisor: Divisor, mutate=False):
                 legals.pop(lIdx)
                 lIdx = 0
             else:
-                lIdx += 1
+                lIdx += 1"""
 
-        graph.lend(divisor, legals, 1)
 
-    if divisor[q] < 0:
-        return False, None, None
-    return True, moves, divisor
+"""
 
+        def choosePivot(lastJ):
+            for j in range(0, len(matrix[0])):
+                for k in range(0 if lastJ == -1 else lastJ + 1, len(matrix)):
+                    if k != 0:
+                        return k, j
+        # Step One
+        lastJ = -1
+        for t in range(0, len(matrix)):
+            j, k = choosePivot(lastJ)
+            if matrix[t][lastJ] == 0:
+                # Exchange rows
+                exchangeRows(t, k)
+            lastJ = j
+            for k in range(0, len(matrix)):
+                if matrix[t][j] % matrix[k][j] != 0:
+                    gcd = math.gcd(matrix[t][j], matrix[k][j])"""
--- main.py
+++ main.py
@@ -3,17 +3,30 @@ from algorithms import *
 
 if __name__ == "__main__":
     adjacency = [
-        [1, 1, 1, 0],
-        [1, 1, 1, 1],
-        [1, 1, 1, 1],
         [0, 1, 1, 1],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
     ]
-    divisor = Divisor([1, -6, 8, -1])
+    """adjacency = [
+        [0, 1, 1],
+        [0, 0, 1],
+        [1, 0, 0],
+    ]"""
+    divisor = Divisor([16, -4, -5, 0])
     graph = Graph(adjacency)
-    print(qReduced(graph, divisor, mutate=True))
-    graph.visualize(divisor)
-    """result = greedy(graph, divisor)
-    print(f'Is winnable: {result[0]}')
-    if result[0]:
-        graph.visualize(result[2])"""
-    # print(dhar(graph, divisor, 0))
+    graph.visualize()
+    # cyclediv = fireCycle(graph, divisor)
+    # print(cyclediv)
+    # grediv = greedy(graph, divisor)
+    # print(grediv)
+    # graph.visualize(grediv[2])
+    # qdiv = qReduced(graph, divisor)
+    # print(qdiv)
+    # graph.visualize(qdiv[2])
+
+    # print(graph.jac(divisor))
+    # print(graph.pic(divisor))
+    for v in range(len(graph)):
+        print(f"Jacobian at vertex {v}:", graph.jac(vertex=v))
+    print("Picard Group:", graph.pic())
